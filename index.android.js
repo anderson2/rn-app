0fd573702839d27af0e580e5b54530b5f4c5ede6
Rob Anderson <rob.anderson2@gmail.com>
2016-10-20 20:55:52 -0700

    Update app home view

--- index.android.js
+++ index.android.js
@@ -1,8 +1,7 @@
 /**
  * React Native App
  *
- * Extended from sample app
- * https://github.com/facebook/react-native
+ * Extended from sample app https://github.com/facebook/react-native
  * @flow
  */
 
@@ -18,9 +17,8 @@ import {
 
 
 export default class rnApp extends Component {
-  render() {
-        let pic = {uri: 'https://upload.wikimedia.org/wikipedia/commons/c/ca/Vincent_Willem_van_Gogh_-_Pont_de_Langlois_-_Kr%C3%B6ller-M%C3%BCller.jpg'};
-
+render() {
+        let pic = {uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Vincent_Willem_van_Gogh_-_Pont_de_Langlois_-_Kr%C3%B6ller-M%C3%BCller.jpg/589px-Vincent_Willem_van_Gogh_-_Pont_de_Langlois_-_Kr%C3%B6ller-M%C3%BCller.jpg'};
 
     return (
       <View style={styles.container}>
@@ -28,16 +26,19 @@ export default class rnApp extends Component {
           React Native App
         </Text>
         <Text style={styles.instructions}>
-          Edited in Android Studio{'\n'}
+          Edited in Android Studio and Codepen.io{'\n'}
         </Text>
-        <Text style={styles.instructions}>
-          v 1.0.3 {'\n'}
+        <Text style={styles.references}>
+          Rob Anderson, 2016 {'\n'}
         </Text>
         
-        <Image source={pic} style={{width: 193, height: 110}}/>
 
+        <View>
+          <Image source={pic} style={{width: 316, height: 258}}/>
+        </View>
+        
         <Text style={styles.instructions}>
-          Langlois Bridge
+          Langlois Bridge - France
         </Text>
 
       </View>
@@ -58,10 +59,21 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   instructions: {
+    fontSize: 14,
     textAlign: 'center',
     color: '#333333',
+    marginTop: 5,    
     marginBottom: 5,
+  },
+  references: {
+    fontSize: 10,
+    textAlign: 'center',
+    margin: 10,
+  },
+  imageContainer: {
+    flex: 1,
   },
 });
 
 AppRegistry.registerComponent('rnApp', () => rnApp);
+
